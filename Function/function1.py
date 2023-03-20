3d5d4aed5fcbc1cbd99d62645de010ee641d30ad
chiranrawal01 <117903075+chiranrawal01@users.noreply.github.com>
2023-03-20 09:05:57 +0545

    python

--- Function/function1.py
+++ Function/function1.py
@@ -24,42 +24,67 @@ def is_prime(num):
 
 #Built a simple Calculator using Functions  (+, - ,*, /)
 
-def sum (a,b):
-    return a+b
+# def sum (a,b):
+#     return a+b
 
 
-def sub (a,b):
-    return a-b
+# def sub (a,b):
+#     return a-b
 
 
-def mul (a,b):
-    return a*b
+# def mul (a,b):
+#     return a*b
 
 
-def div (a,b):
-    return a/b
+# def div (a,b):
+#     return a/b
 
 
-#     print("Selection Operator")
-#     print("Add")
-#     print("Sub")
-#     print("Mul")
-#     print("Divide")
+# #     print("Selection Operator")
+# #     print("Add")
+# #     print("Sub")
+# #     print("Mul")
+# #     print("Divide")
 
-op = input("Enter operator to perform: ")
+# op = input("Enter operator to perform: ")
 
 
+# a =int(input("Enter a Number: "))
+# b=int(input("Enter b number: "))
+# if(op=="+"):
+#     print(f'sum is: {sum(a,b)}')
+# elif(op=='-'):
+#     print(f'sub is {sub(a,b)}')
+# elif(op=='*'):
+#     print(f'mul is {mul(a,b)}')  
+
+# elif(op=='/'):
+#     print(f'div is{div(a,b)}')          
+
+
+'''
+def std_info(name="xyz", section="B"):
+    print(name)
+    print(section)
+
+std_info('asd','C')    '''
+
+'''
+#Create a function to add numbers
 a =int(input("Enter a Number: "))
 b=int(input("Enter b number: "))
+c =int(input("Enter c Number: "))
+d=int(input("Enter d number: "))
+def sum (a,b,c,d):
+    return a+b+c+d
+op = input(" operator to perform: ")
 if(op=="+"):
-    print(f'sum is: {sum(a,b)}')
-elif(op=='-'):
-    print(f'sub is {sub(a,b)}')
-elif(op=='*'):
-    print(f'mul is {mul(a,b)}')  
+  print(f'sum is: {sum(a,b,c,d)}')   '''
 
-elif(op=='/'):
-    print(f'div is{div(a,b)}')          
+'''def sum(*args):
+    sum = 0
+    for i in args:
+        sum += i
+    print(sum) 
 
-
-   
+sum(1,2,3,4,5,6,7,8,9,10) '''   
